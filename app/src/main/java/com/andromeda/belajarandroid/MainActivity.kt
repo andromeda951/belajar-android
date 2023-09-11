6494c235c5045dfd3bbd2c61d4b37d0365433151
Andromeda <andromeda951@gmail.com>
2023-09-12 06:52:06 +0700

    Create Button to change TextView

--- app/src/main/java/com/andromeda/belajarandroid/MainActivity.kt
+++ app/src/main/java/com/andromeda/belajarandroid/MainActivity.kt
@@ -2,10 +2,23 @@ package com.andromeda.belajarandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var text1: TextView
+    private lateinit var btn1: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text1 = findViewById(R.id.text1)
+        btn1 = findViewById(R.id.btn1)
+
+        btn1.setOnClickListener {
+            text1.text = "Hello Programmer"
+        }
     }
 }
