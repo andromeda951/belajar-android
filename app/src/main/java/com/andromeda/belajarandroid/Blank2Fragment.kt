c20cd9582828093b509591ac78069e9f723826c7
Andromeda <andromeda951@gmail.com>
2023-09-21 11:24:18 +0700

    Implement move fragment

--- app/src/main/java/com/andromeda/belajarandroid/Blank2Fragment.kt
+++ app/src/main/java/com/andromeda/belajarandroid/Blank2Fragment.kt
@@ -5,14 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.andromeda.belajarandroid.databinding.FragmentBlank2Binding
 
 class Blank2Fragment : Fragment() {
+
+    private lateinit var binding: FragmentBlank2Binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank2, container, false)
+        // return inflater.inflate(R.layout.fragment_blank2, container, false)
+        binding = FragmentBlank2Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragment1 = Blank1Fragment()
+        binding.btnNext.setOnClickListener {
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, fragment1, Blank1Fragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
